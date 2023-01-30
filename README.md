c9f7b10c17b4bd3328d05385fa847032fe7739ae
Noppakorn Jiravaranun <noppakorn@noppakorn.com>
2023-01-30 16:46:31 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# API for Trainder
+## Requirements
+- [Go](https://go.dev)
+
+## Development
+1. `go mod tidy` to get all the Go requirements 
+2. `go run .` to start the API 
+
+The API will be available at [localhost:8080](http://localhost:8080) 
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/hello", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "hello software engineering 2",
+		})
+	})
+	r.Run()
+}
