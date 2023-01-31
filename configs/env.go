e0f98181f9a80831cde11f528d84cf375e0c9ca2
Noppakorn Jiravaranun <noppakorn@noppakorn.com>
2023-01-31 14:26:04 +0700

    mongo driver setup

---
+++ configs/env.go
@@ -0,0 +1,22 @@
+package configs
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func EnvMongoURI() string {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	return os.Getenv("MONGOURI")
+}
+
+func EnvMongoDBName() string {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	return os.Getenv("MONGO_DATABASE_NAME")
+}
