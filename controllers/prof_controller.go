bd04e7fb3bdeb4cde3dc81c886feb3680021ac2b
nopparuj917 <nopparuj.ta@gmail.com>
2023-02-03 17:04:56 +0700

    add update prof

--- controllers/prof_controller.go
+++ controllers/prof_controller.go
@@ -1,13 +1,21 @@
 package controllers
 
 import (
-	"fmt"
-	"net/http"
+	"trainder-api/configs"
 	"trainder-api/responses"
 	"trainder-api/utils/tokens"
 
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 )
+
+var userCollection *mongo.Collection = configs.GetCollection(configs.DB, "users")
 
 type ProfileInput struct {
 	Firstname   string `json:"firstname" binding:"required"`
@@ -35,6 +43,31 @@ func UpdateProfile() gin.HandlerFunc {
 			c.JSON(http.StatusBadRequest, responses.CurrentUserResponse{
 				Status:  http.StatusBadRequest,
 				Message: err.Error(),
+			})
+			return
+		}
+		//update mongo
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		result,err := userCollection.UpdateOne(
+			ctx,
+			bson.M{"username": Username},
+			bson.M{"$set": bson.M{
+				"firstname":   input.Firstname,
+				"lastname":    input.Lastname,
+				"birthdate":   input.Birthdate,
+				"citizenid":   input.CitizenId,
+				"gender":      input.Gender,
+				"phonenumber": input.PhoneNumber,
+				"address":     input.Address,
+				"subaddress":  input.SubAddress,
+			}},
+		)
+		_ = result
+		if err != nil {
+			c.JSON(http.StatusBadRequest, responses.CurrentUserResponse{
+				Status:  http.StatusBadRequest,
+				Message: `update failed`,
 			})
 			return
 		}
