e9b5bd8bfac1551ffc815c1787be38b37b5ae2e3
s6007431 <looknatc@gmail.com>
2023-02-06 21:23:37 +0700

    .

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	// "fmt"
 	"net/http"
+	// "trainder-api/configs"
 	"trainder-api/routes"
 
 	"github.com/gin-gonic/gin"
+	// "trainder-api/controllers" //nat edited
+	// "go.mongodb.org/mongo-driver/bson"//nat edited
+	// "go.mongodb.org/mongo-driver/mongo" //nat edited
 )
 
 func main() {
@@ -14,8 +19,20 @@ func main() {
 			"data": "Welcome to Trainder API",
 		})
 	})
+	//nat edited
+	// router.POST("/udprof", func(c *gin.Context) {
+	// 	c.JSON(http.StatusOK, gin.H{
+	// 		"data": "xxxxxx Welcome to Trainder API",
+	// 	})
+	// })
+	// var userCollection *mongo.Collection = configs.GetCollection(configs.DB, "usersx")
+	// fmt.Println("printDB", *configs.DB)
+	// fmt.Println("printuser", userCollection.Name(), userCollection.Database().Name())
+	// router.POST("/nao", controllers.UpdateProfile())
+	// router.GET("/nao", controllers.GetProfile())
 
 	routes.AuthRoute(router)
 	routes.ProtectedRoute(router)
-	router.Run(":8080")
+	// router.Run(":8080")
+	router.Run("127.0.0.1:8080")
 }
