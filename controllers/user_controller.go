3ea37127235db70002246fa4fb7584dea51eb65e
Noppakorn Jiravaranun <noppakorn@users.noreply.github.com>
2023-02-07 15:25:47 +0700

    user profile api status fix (#17)

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -101,8 +101,8 @@ func GetProfile() gin.HandlerFunc {
 			return
 		}
 
-		c.JSON(http.StatusFound, responses.GetProfileResponses{
-			Status:  http.StatusFound,
+		c.JSON(http.StatusOK, responses.GetProfileResponses{
+			Status:  http.StatusOK,
 			Message: `Successfully retrieve user profile`,
 			User:    result,
 		})
