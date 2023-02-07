4d88ebd8c9cc4d6c888c748360e83f8c33e611cb
s6007431 <looknatc@gmail.com>
2023-02-07 15:09:35 +0700

    fix main

--- main.go
+++ main.go
@@ -29,23 +29,12 @@ func main() {
 			"data": "Welcome to Trainder API",
 		})
 	})
-	//nat edited
-	// router.POST("/udprof", func(c *gin.Context) {
-	// 	c.JSON(http.StatusOK, gin.H{
-	// 		"data": "xxxxxx Welcome to Trainder API",
-	// 	})
-	// })
-	// var userCollection *mongo.Collection = configs.GetCollection(configs.DB, "usersx")
-	// fmt.Println("printDB", *configs.DB)
-	// fmt.Println("printuser", userCollection.Name(), userCollection.Database().Name())
-	// router.POST("/nao", controllers.UpdateProfile())
-	// router.GET("/nao", controllers.GetProfile())
 
 	routes.AuthRoute(router)
 	routes.ProtectedRoute(router)
 	// router.Run(":8080")
 
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
-	// router.Run(":8080")
-	router.Run("127.0.0.1:8080")
+	router.Run(":8080")
+	// router.Run("127.0.0.1:8080")
 }
