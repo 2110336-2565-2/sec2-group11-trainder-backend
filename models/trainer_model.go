a121bf8e42ab3719a01b3af2b7c04fbedaf1c093
nopparuj917 <nopparuj.ta@gmail.com>
2023-02-19 20:17:34 +0700

    style: math.pi

--- models/trainer_model.go
+++ models/trainer_model.go
@@ -248,7 +248,7 @@ func IsTrainer(username string) (b bool) {
 }
 
 func GetDistance(lat1 float64, lng1 float64, lat2 float64, lng2 float64) float64 {
-	const PI float64 = 3.141592653589793
+	const PI float64 = math.Pi
 	radlat1 := float64(PI * lat1 / 180)
 	radlat2 := float64(PI * lat2 / 180)
 	theta := float64(lng1 - lng2)
