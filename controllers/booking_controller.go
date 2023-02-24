55bce53149406891198267fdaf01b139f1531dcb
nopparuj917 <nopparuj.ta@gmail.com>
2023-02-24 16:43:52 +0700

    feat(booking): implement create-booking

--- controllers/booking_controller.go
+++ controllers/booking_controller.go
@@ -2,19 +2,22 @@ package controllers
 
 import (
 	"net/http"
-	"trainder-api/models"
 	"trainder-api/responses"
 	"trainder-api/utils/tokens"
 
 	"github.com/gin-gonic/gin"
 )
 
-type BookingDetails struct {
+type BookingForm struct {
+	Trainer   string `json:"trainer"`
+	Date      string `json:"date"`
+	StartTime string `json:"startTime"`
+	EndTime   string `json:"endTime"`
 }
 
 func CreateBooking() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		var input UpdateTrainerDetails
+		var input BookingForm
 		if err := c.ShouldBindJSON(&input); err != nil {
 			c.JSON(http.StatusBadRequest, responses.ProfileResponse{
 				Status:  http.StatusBadRequest,
@@ -23,28 +26,19 @@ func CreateBooking() gin.HandlerFunc {
 			return
 		}
 		username, err := tokens.ExtractTokenUsername(c)
-		if err != nil || !models.IsTrainer(username) {
+		if err != nil {
 			c.JSON(http.StatusBadRequest, responses.ProfileResponse{
 				Status:  http.StatusBadRequest,
 				Message: err.Error(),
 			})
 			return
 		}
-		_, err = models.UpdateTrainerProfile(
-			username, input.Specialty, input.Rating,
-			input.Fee, input.TraineeCount, input.CertificateUrl)
-		if err != nil {
-			c.JSON(http.StatusBadRequest, responses.ProfileResponse{
-				Status:  http.StatusBadRequest,
-				Message: `update failed`,
-			})
-			return
-		}
+		_ = username
 
 		c.JSON(http.StatusOK,
 			responses.ProfileResponse{
 				Status:  http.StatusOK,
-				Message: username + ` update success!`,
+				Message: `success!`,
 			})
 	}
 }
